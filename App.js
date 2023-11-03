4d5bc3e29c3bdb00b6cb252590efd4746c446f3b
Dhiraj Mokal <dhirajmokal13@gmail.com>
2023-11-03 13:42:51 +0530

    Add - Movie Card

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NativeWindStyleSheet } from "nativewind";
+import { Home } from './src/screens/Home';
+
+NativeWindStyleSheet.setOutput({
+  web: 'css',
+  default: 'native'
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-gray-200 py-12">
+      <Text className="bg-red-300">Hello</Text>
+        <Home/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/MovieCard.js
@@ -0,0 +1,28 @@
+import { Text, View, Image, SafeAreaView, TouchableOpacity } from 'react-native';
+
+import React from 'react'
+
+const MovieCard = (props) => {
+    return (
+        <SafeAreaView>
+            <View className="relative text-gray-700 overflow-hidden bg-white shadow-md w-[340] rounded-xl my-2">
+                <View className="relative m-5 max-h-[500] text-white shadow-lg bg-blue-gray-500 shadow-blue-gray-500/40">
+                    <Image className="w-full h-full" layout="fill" source={{ uri: props.moviePoster}} />
+                    <Text className="absolute bottom-0 bg-black/[0.65] text-white py-2 px-6">{props.type}</Text>
+                </View>
+                <View className="px-5 pb-3">
+                    <Text className="block mb-2 text-lg antialiased leading-snug tracking-normal text-blue-gray-900">
+                        { props.movieName } <Text className="text-rose-500">({ props.releaseYear })</Text>
+                    </Text>
+                </View>
+                <View className="px-5 pt-0 w-[175] flex flex-row">
+                    <TouchableOpacity className="mb-4">
+                        <Text className="rounded-md bg-pink-600 py-3 px-6 text-center align-middle text-xs font-bold uppercase text-white shadow-md shadow-pink-600/20 transition-all hover:shadow-lg hover:shadow-pink-600/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">Read More ...</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default MovieCard
