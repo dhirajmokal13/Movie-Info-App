b76ed647d20970eb453d7de1cc75709750524b47
Dhiraj Mokal <dhirajmokal13@gmail.com>
2023-11-07 12:36:14 +0530

    Add - Loaders for Cards and Images, Fix - Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,39 +1,51 @@
 import MovieCard from '../components/MovieCard';
-import { ScrollView, SafeAreaView, View, TextInput, Alert, Text, TouchableOpacity } from 'react-native';
+import { ScrollView, SafeAreaView, View, TextInput, Alert, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 const key = '4034acde' || 76593128;
 
-const data = [
-    { Title: 'Spider-Man', Year: '2002', imdbID: 'tt0145487', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg' },
+const defaultData = [
+    { "Title": "Mr. Robot", "Year": "2015–2019", "imdbID": "tt4158110", "Type": "series", "Poster": "https://m.media-amazon.com/images/M/MV5BM2QyNDIzOGMtNThhNS00NmUwLWI0ZjUtZjdkN2I1OTRjZWQ3XkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_SX300.jpg" },
+    { "Title": "Dark", "Year": "2017–2020", "imdbID": "tt5753856", "Type": "series", "Poster": "https://m.media-amazon.com/images/M/MV5BOTk2NzUyOTctZDdlMS00MDJlLTgzNTEtNzQzYjFhNjA0YjBjXkEyXkFqcGdeQXVyMjg1NDcxNDE@._V1_SX300.jpg" },
+    { "Title": "Avengers: Endgame", "Year": "2019", "imdbID": "tt4154796", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg" },
+    { "Title": "Sacred Games", "Year": "2018–2019", "imdbID": "tt6077448", "Type": "series", "Poster": "https://m.media-amazon.com/images/M/MV5BMjJlMjJlMzYtNmU5Yy00N2MwLWJmMjEtNWUwZWIyMGViZDgyXkEyXkFqcGdeQXVyOTAzMTc2MjA@._V1_SX300.jpg" },
     { Title: 'Spider-Man: No Way Home', Year: '2021', imdbID: 'tt10872600', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_SX300.jpg' }, ,
     { Title: 'Spider-Man: Homecoming', Year: '2017', imdbID: 'tt2250912', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BNTk4ODQ1MzgzNl5BMl5BanBnXkFtZTgwMTMyMzM4MTI@._V1_SX300.jpg' },
-    { Title: 'Spider-Man 2', Year: '2004', imdbID: 'tt0316654', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMzY2ODk4NmUtOTVmNi00ZTdkLTlmOWYtMmE2OWVhNTU2OTVkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg' },
-    { Title: 'The Amazing Spider-Man', Year: '2012', imdbID: 'tt0948470', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_SX300.jpg' },
-    { Title: 'Spider-Man: Into the Spider-Verse', Year: '2018', imdbID: 'tt4633694', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_SX300.jpg' },
-    { Title: 'Spider-Man 3', Year: '2007', imdbID: 'tt0413300', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg' },
-    { Title: 'Spider-Man: Far from Home', Year: '2019', imdbID: 'tt6320628', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMGZlNTY1ZWUtYTMzNC00ZjUyLWE0MjQtMTMxN2E3ODYxMWVmXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg' },
-    { Title: 'The Amazing Spider-Man 2', Year: '2014', imdbID: 'tt1872181', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BOTA5NDYxNTg0OV5BMl5BanBnXkFtZTgwODE5NzU1MTE@._V1_SX300.jpg' },
-    { Title: 'Spider-Man: Across the Spider-Verse', Year: '2023', imdbID: 'tt9362722', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMzI0NmVkMjEtYmY4MS00ZDMxLTlkZmEtMzU4MDQxYTMzMjU2XkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_SX300.jpg' }
+    { "Title": "Masaan", "Year": "2015", "imdbID": "tt4635372", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BMTU4NTE0NTMyNl5BMl5BanBnXkFtZTgwNjI5MDkxNjE@._V1_SX300.jpg" },
+    { "Title": "Jai Bhim", "Year": "2021", "imdbID": "tt15097216", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BNzFkM2FhMzQtYjUwZi00N2Y3LWFkZWItMmZmMjQxNGQwZmNhXkEyXkFqcGdeQXVyODEyNjEwMDk@._V1_SX300.jpg" },
+    { "Title": "Newton", "Year": "2017", "imdbID": "tt6484982", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BMDk4Y2Q2MmUtYjE1YS00NjEwLTg0ZGYtOGJkZjUzOTllYzA0L2ltYWdlXkEyXkFqcGdeQXVyNzA0MTg0MzE@._V1_SX300.jpg" },
+    { "Title": "Paatal Lok", "Year": "2020-", "imdbID": "tt9680440", "Type": "series", "Poster": "https://m.media-amazon.com/images/M/MV5BMTE5NWUyMmYtMWE1My00ZDhiLWExZjEtMGJjYTA0OGYwZjIwXkEyXkFqcGdeQXVyODQ5NDUwMDk@._V1_SX300.jpg" },
+    { "Title": "Tandav", "Year": "2021–", "imdbID": "tt11117570", "Type": "series", "Poster": "https://m.media-amazon.com/images/M/MV5BNzRjMjhmZmUtYjRhNS00YjlmLWI4MzUtMDFhM2E0MjAyZGRjXkEyXkFqcGdeQXVyMTEwNDM0NTI2._V1_SX300.jpg" },
+    { "Title": "Jehanabad - Of Love & War", "Year": "2023–", "imdbID": "tt25375470", "Type": "series", "Poster": "https://m.media-amazon.com/images/M/MV5BM2U0OWVkMTAtNDg4NC00NTBhLWFiOGYtMDljNzkyOGRhMzUzXkEyXkFqcGdeQXVyOTIxMzQ0NzU@._V1_SX300.jpg" },
+    { "Title": "Article 15", "Year": "2019", "imdbID": "tt10324144", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BZTI2NTFiMDMtZjQxNS00YjBkLWFhNWMtOTIyMzE5Yjc0ZTZiXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg" },
+    { "Title": "Jawan", "Year": "2023", "imdbID": "tt15354916", "Type": "movie", "Poster": "https://m.media-amazon.com/images/M/MV5BOWI5NmU3NTUtOTZiMS00YzA1LThlYTktNDJjYTU5NDFiMDUxXkEyXkFqcGdeQXVyMTUzNjEwNjM2._V1_SX300.jpg" }
 ]
 
 export const Home = () => {
     const [searchText, setSearchText] = useState('');
-    const [movieResult, setMovieResult] = useState([]);
+    const [movieResult, setMovieResult] = useState(defaultData);
     const [totalResults, setTotalResults] = useState(0);
     const [currentPageNo, setCurrentPageNo] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+    const [firstLoad, setFirstLoad] = useState(true);
 
     useEffect(() => {
-        handleSearch();
-    }, [currentPageNo])
+        if (!firstLoad) {
+            handleSearch();
+        }
+    }, [currentPageNo]);
 
     const handleSearch = () => {
+        setIsLoading(true);
         axios.get(`http://www.omdbapi.com/?apikey=${key}&s=${searchText}&page=${currentPageNo}`).then(res => {
             if (res.data.Response === "True") {
                 setMovieResult(res.data.Search);
                 setTotalResults(res.data.totalResults);
+                setIsLoading(false);
+                setFirstLoad(false);
             } else {
                 Alert.alert("No Result Found");
+                setIsLoading(false);
             }
         }).catch(error => {
             console.log(error);
@@ -67,25 +79,29 @@ export const Home = () => {
         <SafeAreaView>
             <View className="flex-1 items-center justify-center flex-row">
                 <View className="border border-gray-300 bg-gray-300 py-1 px-4 rounded-md w-[65%] mb-2 mt-3 mx-2">
-                    <TextInput placeholder="Search Movies/Series/Shows" value={searchText} onChangeText={txt => setSearchText(txt)} returnKeyType="search" onSubmitEditing={handleSearch} />
+                    <TextInput placeholder="Search Movies/Series/Shows" value={searchText} onChangeText={txt => setSearchText(txt)} returnKeyType="search" onSubmitEditing={() => currentPageNo === 1 ? handleSearch() : setCurrentPageNo(1)} />
                 </View>
                 <Text className="text-lime-800">Results: {totalResults}</Text>
             </View>
+            {
+                firstLoad ? <Text className="mx-8 text-emerald-700 mb-1">Suggested Content</Text> : <Text className="mx-8 text-emerald-700 mb-1">Search Results: {searchText}</Text>
+            }
             <ScrollView horizontal={true}>
                 {
-                    movieResult?.map((item, index) => {
-                        if (item["Poster"] === "N/A") item["Poster"] = "https://previews.123rf.com/images/kaymosk/kaymosk1804/kaymosk180400006/100130939-error-404-page-not-found-error-with-glitch-effect-on-screen-vector-illustration-for-your-design-.jpg"
+                    isLoading ? (<View className="min-h-[120vw] flex-1 justify-center px-[40vw]"><ActivityIndicator size="large" color="#0000ff" /></View>) : movieResult?.map((item, index) => {
+                        if (item["Poster"] === "N/A") item["Poster"] = "https://img.freepik.com/premium-vector/poster-with-inscription-error-404_600765-3956.jpg"
                         return <MovieCard key={index} no={calCulatePostNo(currentPageNo, index + 1)} movieName={item["Title"]} type={item["Type"]} moviePoster={item["Poster"]} releaseYear={item["Year"]} imdbID={item["imdbID"]} />;
                     })
                 }
             </ScrollView>
+
             <View className="px-5 flex flex-row items-center justify-center my-2">
-                <TouchableOpacity className="mr-5" onPress={() => handlePagination("previous")}>
-                    <Text className="w-[25vw] rounded bg-indigo-900 py-3 px-4 text-center align-middle text-xs font-bold uppercase text-white shadow-md shadow-indigo-900/20 transition-all hover:shadow-lg hover:shadow-indigo-900/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">Previous</Text>
+                <TouchableOpacity className="mr-5" disabled={isLoading ? true : false} onPress={() => handlePagination("previous")}>
+                    <Text className={`w-[25vw] rounded ${isLoading ? 'bg-indigo-500' : 'bg-indigo-900'} py-3 px-4 text-center align-middle text-xs font-bold uppercase text-white shadow-md shadow-indigo-900/20 transition-all hover:shadow-lg hover:shadow-indigo-900/60 focus:opacity-[0.90] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none`}>Previous</Text>
                 </TouchableOpacity>
                 <Text>{currentPageNo}</Text>
-                <TouchableOpacity className="ml-5" onPress={() => handlePagination("next")}>
-                    <Text className="w-[25vw] rounded bg-indigo-900 py-3 px-4 text-center align-middle text-xs font-bold uppercase text-white shadow-md shadow-indigo-900/20 transition-all hover:shadow-lg hover:indigo-900/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">Next</Text>
+                <TouchableOpacity className="ml-5" disabled={isLoading ? true : false} onPress={() => handlePagination("next")}>
+                    <Text className={`w-[25vw] rounded ${isLoading ? 'bg-indigo-500' : 'bg-indigo-900'} py-3 px-4 text-center align-middle text-xs font-bold uppercase text-white shadow-md shadow-indigo-900/20 transition-all hover:shadow-lg hover:indigo-900/60 focus:opacity-[0.90] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none`}>Next</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
